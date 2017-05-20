7b18f57b4122a692296c31799c30c2e99e48baa3
Oshane Thomas <othomas2@illinois.edu>
2017-05-19 21:24:14 -0500

    Update data_loader.py

--- kaos/data/data_loader.py
+++ kaos/data/data_loader.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pickle as pkl
-import os, urllib, gzip
+import os, gzip
 import scipy.io
 
 class StandardDataLoader(object):
@@ -50,7 +50,7 @@ class MnistLoader(StandardDataLoader):
         if not os.path.exists(data_loc):
             url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
             print("Downloading data from:", url)
-            urllib.urlretrieve(url, data_loc)
+            data_loc = urllib.request.urlretrieve(url)
         return data_loc
 
     def _load_mnist(self):
